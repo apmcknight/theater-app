7ebed7eb6f6893f6b0c0e032f9c91267392c0d03
Adam McKnight <adammcknight@me.com>
2020-11-20 22:42:35 -0500

    General Commit_0

---
+++ src/TheaterApp/TheaterApp.Client/wwwroot/main.js
@@ -0,0 +1,14 @@
+// var getCurrentTime = function() {
+//     var date = new Date();
+//     var hours =  date.getHours();
+//     var minutes =  date.getMinutes();
+//     var current = hours + (minutes * .01);
+//     if (current == null) return 'Hello,';
+//     if (current >= 5 && current < 19) return 'Good Morning,';
+//     if (current >= 19 && current < 22) return 'Good Afternoon,';
+//     if (current >= 22 || current < 5) return 'Good Evening,';
+// }
+
+
+// let greeting = document.querySelectorAll("greetingText");
+// document.write("Hello trucker!")
