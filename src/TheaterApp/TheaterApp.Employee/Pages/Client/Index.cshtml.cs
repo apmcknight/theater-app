c0d1818dfc6a212f52a25a9b6201de28af4edc54
Adam McKnight <adammcknight@me.com>
2021-03-12 22:50:01 -0500

    Added a new dir for the Client side site, and added a layout view for the client.

---
+++ src/TheaterApp/TheaterApp.Employee/Pages/Client/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TheaterApp.Employee.Pages
+{
+    public class ClientModel : PageModel
+    {
+        private readonly ILogger<ClientModel> _logger;
+
+        public ClientModel(ILogger<ClientModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
